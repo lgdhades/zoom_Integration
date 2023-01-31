2de6ae8b0a94c1c898c02cb877aff2b813dd6521
lgdhades <hades987456@outlook.com>
2023-01-31 16:37:36 -0500

    connect local mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,30 @@
-require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-// const connection = require("./db");
-// const Api = require("./api/Api");
+const mongoose = require("mongoose");
+
+// const users = require("./routes/api/users");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-var corsOptions = {
-  origin: "http://localhost:8081",
-};
-
-// database connection
-// connection();
-
-// middleware
-app.use(express.urlencoded({ extended: true }));
+// BodyParser Middleware
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(express.json());
-app.use(cors());
-app.use((req, res, next) => {
-  res.locals.path = req.path;
-  next();
-});
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
+// DB config
+const db = require("./config/keys.js").mongoURI;
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// connect to mongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
+// // Routes
+// app.use("/api/users", users);
 
-// set port, listen for requests
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
